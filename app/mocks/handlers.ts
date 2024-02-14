48708526ed16754de663ece57e3fa3d5a6ddd490
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2024-02-14 12:25:45 +0100

    Use MSW to Mock Authentication

--- app/mocks/handlers.ts
+++ app/mocks/handlers.ts
@@ -84,4 +84,23 @@ export const handlers = [
     const recommendations = movies.filter((movie) => movie.id !== movieId);
     return HttpResponse.json(recommendations);
   }),
+  http.post('https://auth.provider.com/validate', async ({ request }) => {
+    const data = await request.formData();
+    const email = data.get('email');
+    const password = data.get('password');
+
+    if (!email || !password) {
+      return new HttpResponse('', { status: 400 });
+    }
+
+    const user = {
+      id: '2b225b31-904a-443b-a898-a280fa8e0356',
+      email,
+      firstName: 'John',
+      lastName: 'Maverick',
+      avatarUrl: 'https://i.pravatar.cc/100?img=12',
+    };
+
+    return HttpResponse.json(user);
+  }),
 ];
